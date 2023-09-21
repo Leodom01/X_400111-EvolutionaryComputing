8367d4c09fdd72ae397a437d66685c1543da3e1c
Quartinocci <96389802+Quartinocci@users.noreply.github.com>
2023-09-21 06:35:20 +0200

    Add files via upload

---
+++ evoman_framework/run_them_all.py
@@ -0,0 +1,9 @@
+import genetic_algorithm_1
+import subprocess
+
+
+genetic_algorithm_1.run_experiments(2, 1, True)
+
+genetic_algorithm_1.run_experiments(2, 1, False)
+
+#subprocess.call(["python", "plot.py"])
