4951ecc787f97314e51af726e591a93a3303a322
Quartinocci <96389802+Quartinocci@users.noreply.github.com>
2023-09-21 06:41:50 +0200

    Update run_them_all.py

--- evoman_framework/run_them_all.py
+++ evoman_framework/run_them_all.py
@@ -1,9 +1,10 @@
 import genetic_algorithm_1
 import subprocess
 
-
+#make 2 runs against enemy 1 with 2point crossover
 genetic_algorithm_1.run_experiments(2, 1, True)
 
+#make 2 runs against enemy 1 with whole aritm crossover
 genetic_algorithm_1.run_experiments(2, 1, False)
 
 #subprocess.call(["python", "plot.py"])
