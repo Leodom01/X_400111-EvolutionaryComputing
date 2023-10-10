2925c88a05583fe77a96e56d1b3c1dedd50c8416
Leonardo Dominici <leonardodominici01@gmail.com>
2023-10-10 13:41:23 +0200

    Fix for multithreading on MacOS/Win

--- task2/train.py
+++ task2/train.py
@@ -22,11 +22,13 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = 'hide'
 
 neuron_number, env = training_environment(ENEMIES)
 
+
 def evaluate(phenone):
+
   def run_single(enemy):
     f, p, e, _ = env.run_single(pcont=phenone, enemyn=enemy, econt=None)
     return f, p, e
-  
+
   fitnesses = []
   kills = 0
   deaths = 0
@@ -37,50 +39,58 @@ def evaluate(phenone):
     if p == 0: deaths += 1
     if p != 0 and e != 0: out_of_time += 1
 
-  classic_fitness = np.average(fitnesses) - np.std(fitnesses) 
+  classic_fitness = np.average(fitnesses) - np.std(fitnesses)
   f = classic_fitness + 50 * kills - 30 * out_of_time - 30 * deaths
   return -f
 
-# init = [0] * neuron_number
-init = np.loadtxt("./w.txt")
+def main():
 
-engine = cma.CMAEvolutionStrategy(
-  init,
-  INITIAL_SIGMA,
-  {
-    "popsize": NPOP,
-  }
-)
+  init = [0] * neuron_number
+  # init = np.loadtxt("./w.txt")
 
-try:
-  cpus = multiprocessing.cpu_count()
-except NotImplementedError:
-  cpus = 1
-pool = multiprocessing.Pool(processes=cpus)
-
-# for i in range(NGEN):
-i = 0
-while i < NGEN:
-  solutions = engine.ask()
-  fitness = list(pool.map(evaluate, solutions))
-  engine.tell(solutions, fitness)
-
-  current_best_fitness = - np.min(fitness)
-  average_fitness = - np.average(fitness)
-  global_best = - engine.result[1]
-
-  print(
-    f"Generation {i}\t"
-    f"max fitness: {current_best_fitness}\t"
-    f"average fitness: {average_fitness}\t"
-    f"best of all time: {global_best}"
+  engine = cma.CMAEvolutionStrategy(
+    init,
+    INITIAL_SIGMA,
+    {
+      "popsize": NPOP,
+    }
   )
 
-  np.savetxt(f"tmp-agent.txt", engine.result[0])
+  try:
+    cpus = multiprocessing.cpu_count()
+  except NotImplementedError:
+    cpus = 1
 
-  i += 1
+  pool = multiprocessing.Pool(processes=cpus)
 
-np.savetxt(f"agent--fit-{engine.result[1]}.txt", engine.result[0])
-pool.close()
-# np.savetxt(f"agent-custom-fitness-fit-{engine.result[1]}.txt", engine.result[0])
-# np.savetxt(f"agent-custom-enemies-fit-{engine.result[1]}.txt", engine.result[0])
+  # for i in range(NGEN):
+  i = 0
+  while i < NGEN:
+    solutions = engine.ask()
+    fitness = list(pool.map(evaluate, solutions))
+    engine.tell(solutions, fitness)
+
+    current_best_fitness = - np.min(fitness)
+    average_fitness = - np.average(fitness)
+    global_best = - engine.result[1]
+
+    print(
+      f"Generation {i}\t"
+      f"max fitness: {current_best_fitness}\t"
+      f"average fitness: {average_fitness}\t"
+      f"best of all time: {global_best}"
+    )
+
+    np.savetxt(f"tmp-agent.txt", engine.result[0])
+
+    i += 1
+
+  np.savetxt(f"agent--fit-{engine.result[1]}.txt", engine.result[0])
+  pool.close()
+  # np.savetxt(f"agent-custom-fitness-fit-{engine.result[1]}.txt", engine.result[0])
+  # np.savetxt(f"agent-custom-enemies-fit-{engine.result[1]}.txt", engine.result[0])
+
+
+if __name__ == "__main__":
+  multiprocessing.freeze_support()
+  main()
