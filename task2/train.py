baa1aa8a962a0440a2c68e23cc3f2fb28b253584
AlecsFerra <alecsferra@protonmail.com>
2023-10-12 01:28:16 +0200

    Add weighted fitness

--- task2/train.py
+++ task2/train.py
@@ -2,13 +2,13 @@ import os
 import cma
 import multiprocessing
 import numpy as np
-
+from functools import partial
 from environment import training_environment
 
 # BEGIN META PARAMETERS
 # ENEMIES = [1, 3, 4, 6, 7]
 ENEMIES = range(1, 9)
-NGEN = 10000
+NGEN = 300
 # END META PARAMETERS
 
 # BEGIN HYPER PARAMETERS
@@ -22,26 +22,63 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = 'hide'
 
 neuron_number, env = training_environment(ENEMIES)
 
+def run_single(phenome, enemy):
+  f, p, e, _ = env.run_single(pcont=phenome, enemyn=enemy, econt=None)
+  return f, p, e
 
-def evaluate(phenone):
+def run(phenome):
+  return list(map(partial(run_single, phenome), ENEMIES))
 
-  def run_single(enemy):
-    f, p, e, _ = env.run_single(pcont=phenone, enemyn=enemy, econt=None)
-    return f, p, e
+def compute_weights(runs):
+  tot_enemy_gain = [0] * len(ENEMIES)
+  for run in runs:
+    for enemy_number, (_, p_energy, e_energy) in enumerate(run):
+      tot_enemy_gain[enemy_number] += (e_energy - p_energy)
 
-  fitnesses = []
-  kills = 0
-  deaths = 0
-  out_of_time = 0
-  for (f, p, e) in map(run_single, ENEMIES):
-    fitnesses.append(f)
-    if e == 0: kills += 1
-    if p == 0: deaths += 1
-    if p != 0 and e != 0: out_of_time += 1
+  tot_gain = sum(tot_enemy_gain)
+  return list(map(lambda gain: gain / tot_gain, tot_enemy_gain))
 
-  classic_fitness = np.average(fitnesses) - np.std(fitnesses)
-  f = classic_fitness + 50 * kills - 30 * out_of_time - 30 * deaths
-  return -f
+def compute_fitness(runs, weights, generation_number):
+  weights = compute_weights(runs)
+
+  def compute(run):
+    fitnesses = []
+    n_kills = 0
+    n_deaths = 0
+    n_timeouts = 0
+    for f, p_energy, e_energy in run:
+      fitnesses.append(f)
+      if p_energy == 0:
+        n_deaths += 1
+      if e_energy == 0:
+        n_kills += 1
+      if p_energy != 0 and e_energy != 0:
+        n_timeouts += 1
+    
+    base_fit = np.average(fitnesses, weights=weights)
+    return - base_fit
+
+  return list(map(compute, runs))
+
+def compute_stats(runs):
+  def compute(run):
+    fitnesses = []
+    n_kills = 0
+    n_deaths = 0
+    n_timeouts = 0
+    for f, p_energy, e_energy in run:
+      fitnesses.append(f)
+      if p_energy == 0:
+        n_deaths += 1
+      if e_energy == 0:
+        n_kills += 1
+      if p_energy != 0 and e_energy != 0:
+        n_timeouts += 1
+    
+    base_fit = np.average(fitnesses) - np.std(fitnesses)
+    return (- base_fit, n_kills)
+
+  return list(map(compute, runs))
 
 def main():
   init = [0] * neuron_number
@@ -63,23 +100,38 @@ def main():
 
     for i in range(NGEN):
       solutions = engine.ask()
-      fitness = list(pool.map(evaluate, solutions))
+      
+      results = pool.map(run, solutions)
+     
+      stats = compute_stats(results)
+      classical_fitness, kills = map(list, zip(*stats))
+
+      weights = compute_weights(results)
+      fitness = compute_fitness(results, weights, i)
+
       engine.tell(solutions, fitness)
 
+      max_kills = np.max(kills)
       current_best_fitness = - np.min(fitness)
-      average_fitness = - np.average(fitness)
+      current_average_fitness = - np.average(fitness)
+      current_best_classical_fitness = - np.min(classical_fitness)
+      current_average_classical_fitness = - np.average(classical_fitness)
+
       global_best = - engine.result[1]
 
       print(
         f"Generation {i}\t"
-        f"max fitness: {current_best_fitness}\t"
-        f"average fitness: {average_fitness}\t"
+        f"max fitness: {current_best_fitness} "
+        f"(classic: {current_best_classical_fitness})\t"
+        f"average fitness: {current_average_fitness} "
+        f"(classic: {current_average_classical_fitness})\t"
+        f"kills: {max_kills}\t"
         f"best of all time: {global_best}"
       )
 
       np.savetxt(f"tmp-agent.txt", engine.result[0])
 
-    np.savetxt(f"agent--fit-{engine.result[1]}.txt", engine.result[0])
+    np.savetxt(f"agent-weights-fit-{engine.result[1]}.txt", engine.result[0])
 
 if __name__ == "__main__":
   multiprocessing.freeze_support()
