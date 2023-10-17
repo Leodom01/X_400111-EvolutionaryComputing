7e4be0cd3fb1217ec7681470bb7dc1c8822ce352
AlecsFerra <alecsferra@protonmail.com>
2023-10-17 18:57:03 +0200

    Preparing the final run

--- task2/run-all.py
+++ task2/run-all.py
@@ -2,7 +2,7 @@ import os
 
 for enemy in ["1-2-3-4-5-6-7-8", "1-2-6"]:
     for fitness in ["custom"]:
-        for run in range(5):
+        for run in range(10):
             print(f"Run {fitness} {enemy} {run}")
             os.environ["FITNESS_FUNCTION"] = fitness
             os.environ["N_RUN"] = str(run)
